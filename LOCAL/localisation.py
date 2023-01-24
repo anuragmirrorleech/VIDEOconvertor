b64226561b3c55e36f64698e090137739290f8b6
anuragmirrorleech <115845369+anuragmirrorleech@users.noreply.github.com>
2023-01-25 04:05:57 +0530

    Update localisation.py

--- LOCAL/localisation.py
+++ LOCAL/localisation.py
@@ -5,13 +5,13 @@ START_TEXT = "Send me any file to begin."
 
 FORCE_SUB_TEXT = "In order to use this bot, you've to join my parent channel."
 
-CHANNEL_LINK = "https://t.me/DroneBots"
+CHANNEL_LINK = "https://t.me/AMROBOTS_BOTS"
 
-SUPPORT_LINK = "https://t.me/TeamDrone"
+SUPPORT_LINK = "https://t.me/AMROOTS_SUPPORT"
 
-info_text = "This bot is developed by @MaheshChauhan\n\nWritten in python library TELETHON.\n\nBot by : @DroneBots\nSupport : @TeamDrone\n\nV1.4"   
+info_text = "This bot is developed by @AM_ROBOTS\n\nWritten in python library TELETHON.\n\nBot by : @Amrobots_Bots\nSupport : @Amrobots_Support\n\nV2.4"   
 
-help_text = """**v1.4**
+help_text = """**v2.4**
 
 •`Encode` - encode your video into different lib format or resolution
 
@@ -29,17 +29,17 @@ help_text = """**v1.4**
 
 source_text = "**Deploy your own bot**"
 
-DEV = "https://t.me/MaheshChauhan"
+DEV = "https://t.me/AM_ROBOTS"
 
-spam_notice = "This bot is hosted on heroku, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
+spam_notice = "This bot is hosted on AM-ROBOTS-SERVER, and hence can just run one process at a time.Spamming the bot or encoding adult videos will lead you to a ban."
 
-JPG = "LOCAL/video_convertor.jpg"
+JPG = "LOCAL/video_convertore.jpg"
 
-JPG0 = 'https://telegra.ph/file/d98c559b56ef884ef3bad.jpg'
+JPG0 = 'https://telegra.ph/file/8619a6f258621134b7576.jpg'
 
-JPG2 = "LOCAL/20211215_165751.jpg"
+JPG2 = "LOCAL/20211215_1657519.jpg"
 
 JPG3 = "LOCAL/PicsArt_12-16-08.57.15.jpg"
 
-JPG4 = "LOCAL/20211219_000258.jpg"
+JPG4 = "LOCAL/20211219_0002589.jpg"
 
